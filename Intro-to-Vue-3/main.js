7a3ada8d8f3cf865252904d945a1f1d611249a22
josefdzbeta <jafb19@outlook.com>
2022-02-10 00:34:23 +0100

    computed

--- Intro-to-Vue-3/main.js
+++ Intro-to-Vue-3/main.js
@@ -1,22 +1,21 @@
-const product = 'Calcetines'
-
 //Datos que recibe la aplicación de Vue
 
 const app = Vue.createApp({
     data() {
         return {
             producto: 'Calcetines',
-            descripcion: 'De puro hilo de humo',
-            imagen: 'assets/images/socks_blue.jpg',
+            marca: 'Yupi',
+            descripcion: 'Puro algodón Egipcio',
+            varianteSeleccionada: 0,
             tooltip: 'Sin agujeros',
             url: 'https://es.wikipedia.org/wiki/Calcet%C3%ADn',
-            stock: 3,
+            stock: 5,
             unidades: 0,
             oferta: true,
             detalles: ['50% algodón', '30% lana', '20% polyester'],
             variantes: [
-                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', stock: 50},
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', stock: 0 },
             ],
             tallas: [
                 { id: 1, tamanyo: 'S' },
@@ -34,9 +33,28 @@ const app = Vue.createApp({
                 this.unidades--
             }
         },
-        actualizarImagen(varianteImage){
-            this.imagen = varianteImage
+        actualizarVariante(index){
+            this.varianteSeleccionada = index
         }
+    },
+    computed:{
+        title(){
+            return this.marca + ' ' + this.producto
+        },
+        image(){
+            return this.variantes[this.varianteSeleccionada].image
+        },
+        enStock(){
+            return this.variantes[this.varianteSeleccionada].stock
+        },
+        enOferta(){
+            if(this.oferta){
+
+                return this.marca + ' ' + this.producto + ' en oferta: compra un calcetín y llévate un zapato' 
+            }
+        }
+
+        
     }
 })
 
